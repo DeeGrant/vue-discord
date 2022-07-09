8d4c08fb35cadb6f68bb9456896cea61debcffd1
Dee Grant <WalterGrant@gmail.com>
2022-07-09 13:47:17 -0500

    add sidebar icon

--- src/components/SideBar.vue
+++ src/components/SideBar.vue
@@ -1,14 +1,19 @@
 <template>
 <section class="fixed top-0 left-0 h-screen w-16 flex flex-col bg-gray-900 text-white shadow-lg">
- <i>A</i>
- <i>B</i>
- <i>C</i>
- <i>D</i>
+  <side-bar-icon :fa-class="'fa fa-fire-flame-curved'"></side-bar-icon>
+  <side-bar-icon :fa-class="'fa fa-plus'"></side-bar-icon>
+  <side-bar-icon :fa-class="'fa fa-bolt-lightning'"></side-bar-icon>
+  <side-bar-icon :fa-class="'fa fa-poo'"></side-bar-icon>
 </section>
 </template>
 
 <script>
+import SideBarIcon from "@/components/SideBarIcon";
+
 export default {
-  name: "SideBar"
+  name: "SideBar",
+  components: {
+    SideBarIcon
+  }
 }
 </script>
---
+++ src/components/SideBarIcon.vue
@@ -0,0 +1,12 @@
+<template>
+<div class="sidebar-icon">
+  <i :class="faClass"></i>
+</div>
+</template>
+
+<script>
+export default {
+  name: "SideBarIcon",
+  props: ['faClass']
+}
+</script>
